9a2d52f929c9e1d2393eed2b399840bc065a14d8
Karime <L19100252@nlaredo.tecnm.mx>
2022-11-30 23:23:04 -0700

    .

---
+++ Parcial3/Cookies/cookies.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_COOKIE['ultimavisita']))
+{
+    printf("Su ultima visita fue: %s",$_COOKIE['ultimavisita']);
+
+    $fechahoy = date('Y/m/d H:i:s');
+    setcookie("ultimavisita",$fechahoy,time() + (7 * 24 * 60 * 60));
+}
+else{
+    printf("No se encontro cookie de ultima visita<br>");
+
+    $fechahoy = date('Y/m/d H:i:s');
+    $siguienteSemana = time() + (7 * 24 * 60 * 60);
+    $fechaExpira = date('Y/m/d H:i:s',$siguienteSemana).'<br>';
+
+    echo '<br>Fecha de creacion de cookie: <br>'. $fechahoy.'<br>';
+    echo '<br>Fecha de Vencimiento de cookie: <br>'. $fechaExpira.'<br>';
+
+    setcookie("ultimavisita".$fechahoy,time() + (7 * 24 * 60 * 60));
+}
+?>
